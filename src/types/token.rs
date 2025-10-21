574ae5d55d70334cca256150d03bf448b9c5fc5a
algebraicMatroid <54645583+algebraicMatroid@users.noreply.github.com>
2025-10-21 12:29:10 -0500

    add token type

---
+++ src/types/token.rs
@@ -0,0 +1,11 @@
+use alloy::primitives::Address;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Token {
+    pub spoke_address: Address,
+    pub mtoken_address: Address,
+    pub spoke_chain_id: u64,
+    pub symbol: String,
+    pub decimals: u8,
+}
