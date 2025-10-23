a0c3076c70984eab049d17ae890e00948afe2bee
Vivgor <mount26593@proton.me>
2025-10-23 11:04:39 +0800

    feat: one step fast withdraw mtoken

--- src/client/spoke.rs
+++ src/client/spoke.rs
@@ -1,5 +1,5 @@
 use alloy::network::{EthereumWallet, TxSigner};
-use alloy::primitives::{Address, B256, ChainId, U256};
+use alloy::primitives::{Address, B256, Bytes, ChainId, U256};
 use alloy::providers::fillers::{
     BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller, WalletFiller,
 };
@@ -9,6 +9,7 @@ use alloy::signers::{Signature, Signer};
 use crate::error::Error;
 use crate::types::sol_types::AssetReserves::AssetReservesInstance;
 use crate::types::sol_types::ERC20::ERC20Instance;
+use crate::types::sol_types::FastWithdrawalPermit;
 
 type EthereumProvider = FillProvider<
     JoinFill<
@@ -70,6 +71,24 @@ impl SpokeClient {
             .get_receipt()
             .await?;
 
+        Ok(receipt.transaction_hash)
+    }
+
+    pub async fn withdraw_with_permit(
+        &self,
+        permit: FastWithdrawalPermit,
+        receiver: Address,
+        user_signature: Bytes,
+        operator_signature: Bytes,
+    ) -> Result<B256, Error> {
+        let asset_reserves_contract =
+            AssetReservesInstance::new(self.asset_reserves_address, self.provider.clone());
+        let receipt = asset_reserves_contract
+            .withdrawWithPermit(permit, receiver, user_signature, operator_signature)
+            .send()
+            .await?
+            .get_receipt()
+            .await?;
         Ok(receipt.transaction_hash)
     }
 }
